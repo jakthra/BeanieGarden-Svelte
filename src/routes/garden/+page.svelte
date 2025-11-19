afe01193646ef650fae6f33d36c85b9aa27443d2
Jakob Thrane <jthr@tdcnet.dk>
2025-11-19 06:49:18 +0100

    Svelte approach

---
+++ src/routes/garden/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { FenceIcon, PlusIcon } from '@lucide/svelte';
+</script>
+
+<div class="p-8">
+	<div class="flex items-center justify-between">
+		<div class="flex flex-row items-center gap-4">
+			<FenceIcon class="h-full" />
+			<h1 class="text-2xl">Garden</h1>
+		</div>
+		<div class="flex">
+			<button type="button" class="btn preset-filled-primary-500"
+				><PlusIcon size={'1rem'} /> Add</button
+			>
+		</div>
+	</div>
+</div>
---
+++ src/routes/tasks/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { FenceIcon, ListTodoIcon, PlusIcon } from '@lucide/svelte';
+</script>
+
+<div class="p-8">
+	<div class="flex items-center justify-between">
+		<div class="flex flex-row items-center gap-4">
+			<ListTodoIcon class="h-full" />
+			<h1 class="text-2xl">Tasks</h1>
+		</div>
+	</div>
+</div>
